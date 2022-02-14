8bcafc55bafc4d5663c0b8db986c62d90703825e
kewlaniyash10 <90568997+kewlaniyash10@users.noreply.github.com>
2022-02-13 22:42:32 -0500

    Update issue.py

--- issue.py
+++ issue.py
@@ -4,9 +4,9 @@ conn = psycopg2.connect("postgresql://admin:ilovesql@localhost/project")
 cur = conn.cursor()
 
 while True:
+    department = input("Enter department name: ")
     print("Please enter issue ID: ")
     issue_id = int(input())
-    department = input("Enter department name: ")
     print("Fetching Results... ")
     cur.execute ('''
     SELECT * FROM %s WHERE issue = %s RETURNING issue, department, first_name, last_name; 
