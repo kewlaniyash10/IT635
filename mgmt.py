353b9e3453957305231133ac32ab58df9ca07b6e
kewlaniyash10 <90568997+kewlaniyash10@users.noreply.github.com>
2022-02-14 17:52:57 -0500

    Update mgmt.py

--- mgmt.py
+++ mgmt.py
@@ -6,18 +6,24 @@ cur = conn.cursor()
 while True:
     print("Issue Management System \n")
     
-    department = input("Enter Department Name: \n")
+    department = input("Enter Department ID: \n 1. Human Resources \n 2. Finance \n")
     
     issue_id = input("Enter Issue Number: \n")
     
     print("Fetching Results... \n")
     
-    cur.execute ("""
-    SELECT issue, department, first_name, last_name FROM '%s' WHERE issue = %s; 
-    """ ,  (department, issue_id));
+    if department == 1:
+        cur.execute ("""
+        SELECT issue, department, first_name, last_name FROM HR WHERE issue = %s; 
+        """ ,  (issue_id));
+    else:
+        cur.execute ("""
+        SELECT issue, department, first_name, last_name FROM FINANCE WHERE issue = %s; 
+        """ ,  (issue_id));
     
     result = cur.fetchall();
     print(result)
+    
     print("End of Result \n")
     conn.commit()
 
